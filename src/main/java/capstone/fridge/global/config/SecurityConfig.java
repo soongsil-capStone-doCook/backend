cfa3ea934a0a836e165058002045539c628284ea
Donghyeon <dlehdgus1207@naver.com>
2025-12-28 15:56:37 +0900

    cors 설정 추가 2

--- src/main/java/capstone/fridge/global/config/SecurityConfig.java
+++ src/main/java/capstone/fridge/global/config/SecurityConfig.java
@@ -19,9 +19,10 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
                     corsConfiguration.setAllowedOrigins(java.util.List.of("http://localhost:5173"));
-                    corsConfiguration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfiguration.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
                     corsConfiguration.setAllowedHeaders(java.util.List.of("*"));
                     corsConfiguration.setAllowCredentials(true);
+                    corsConfiguration.setExposedHeaders(java.util.List.of("Authorization"));
                     return corsConfiguration;
                 }))
                 .csrf(csrf -> csrf.disable())
